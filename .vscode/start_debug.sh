5508a9685aa51bfe27ffb931e95ec3ca3b6d22be
Francois-Leonard Gilbert <gilbertf@ieee.org>
2025-05-29 19:52:55 -0400

    Branch to create a pull request for QR code rendering

--- .vscode/start_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Start CUPS
-/usr/sbin/cupsd
-lpadmin -p lpd_escpos -v $DEVICE_URI -E -o printer-is-shared=true
--- tests/Jetdirect/test_jetdirect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This is a script to test the jetdirect backend
-python3 ./test_jetdirect.py
